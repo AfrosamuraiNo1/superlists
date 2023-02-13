8a4adefb9de4942eac6d61d767139ea2d7202ac8
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-13 10:45:53 +0300

    First module test and url.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,10 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
-class SmokeTest(TestCase):
-#'''тест на токсичность'''
-    def test_bad_maths(self):
-#'''тест: неправильные матема'''
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+#'''тест домашней страницы'''
+    def test_root_url_resolves_to_home_page_view(self):
+#'''тест: корневой url преобразуется в представление домашней страницы'''
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Создайте здесь представления свои.
+def home_page():
+    pass
