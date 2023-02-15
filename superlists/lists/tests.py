0d06d1ab29d6f8f6576c287c814f5ec75e670d3f
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-15 10:15:34 +0300

    Base views , minimal HTML(3. 1path)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -8,3 +10,11 @@ class HomePageTest(TestCase):
 #'''тест: корневой url преобразуется в представление домашней страницы'''
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+    def test_home_page_returns_correct_html(self):
+#'''тест: домашняя страница возвращает правильный html'''
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
