9e32a3e52fca6547dbe666922f49208cd81a3b8b
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-19 10:59:47 +0300

    update func test(5 path)

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,6 +10,12 @@ class HomePageTest(TestCase):
         '''тест: используется домашний шаблон'''
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        '''тест: можно сохранить post-запрос'''
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
     def test_home_page_returns_correct_html(self):
         '''тест: домашняя страница возвращает правильный html'''
