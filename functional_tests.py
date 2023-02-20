271199bbbca82d08a165d387db24a327d6cb25f6
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-20 10:32:55 +0300

    update func test(5.1 path)

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         '''демонтаж'''
         self.browser.quit()
-        
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''тест: можно начать список и получить его позже'''
         # Эдит слышала про крутое новое онлайн-приложение со
@@ -37,27 +37,31 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(2)
-
-        table = self.browser.find_element('id','id_list_table')
-        rows = table.find_elements('tag name','tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        # Текстовое поле по-прежнему приглашает ее добавить еще один элемент. Она
-        # вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        # Текствое поле по-прежнему приглашает ее добавить еще один элемент.
+        # Она вводит "Сделать мушку из павлиньих перьев"
+        # (Эдит очень методична)
         inputbox = self.browser.find_element('id','id_new_item')
         inputbox.send_keys('Сделать мушку из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(1)
         # Страница снова обновляется и теперь показывает оба элемента ее списка
-        table = self.browser.find_element('id','id_list_table')
-        rows = table.find_elements('tag name','tr')
-        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку из павлиньих перьев',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
+
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с пояснениями.
         self.fail('Закончить тест!')
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
+
+    def check_for_row_in_list_table(self, row_text):
+        '''подтверждение строки в таблице списка'''
+        table = self.browser.find_element('id','id_list_table')
+        rows = table.find_elements('tag name','tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
