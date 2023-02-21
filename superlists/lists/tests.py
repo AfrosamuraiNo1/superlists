a5e34b5fddf07b3365ea5bbcea3e7be80b88df55
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-21 10:42:39 +0300

    Model for List Items and Related Migrat(5.2 path )

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -25,3 +26,20 @@ class HomePageTest(TestCase):
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith('</html>'))
+
+class ItemModelTest(TestCase):
+    '''тест модели элемента списка'''
+    def test_saving_and_retrieving_items(self):
+        '''тест сохранения и получения элементов списка'''
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
