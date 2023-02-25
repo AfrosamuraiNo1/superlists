43f626ed9e7e63057fc7923fc4a56d8396caa908
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-25 10:21:58 +0300

    make func_tests an app, use LiveServerTest(6 path)

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lists import views
+from lists import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
