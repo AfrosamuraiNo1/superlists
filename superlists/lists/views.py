83cde50dad28819886336c1962c791782b970dcf
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-02-25 10:05:38 +0300

    POST show  all items in template(5.4 path)

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from lists.models import Item
+from .models import Item
 
 def home_page(request):
     '''домашняя страница'''
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
+        item = Item.objects.create(text=request.POST['item_text'])
+        item.save()
         return redirect('/')
     items = Item.objects.all()
     return render(request, 'home.html', {'items': items})
