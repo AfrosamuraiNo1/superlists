a07ad37a1a1fe5ca03fccd2cfe73514fe29c30ad
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-03-23 18:26:24 +0300

    Update settings and views make decorator(9.2 path)

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Item, List
+from django.views.decorators.csrf import csrf_exempt
+
 
 def home_page(request):
     '''домашняя страница'''
@@ -10,6 +12,7 @@ def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     return render(request, 'list.html', {'list': list_})
 
+@csrf_exempt
 def new_list(request):
     '''новый список'''
     list_ = List.objects.create()
@@ -17,6 +20,7 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
+@csrf_exempt
 def add_item(request, list_id):
     '''добавить элемент'''
     list_ = List.objects.get(id=list_id)
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-h27g5glrn5v_t&ho06k2513oa_ym1!fn(=a89ve@jg%xv7$wat
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['slistspb.fun', 'localhost']
 
 
 # Application definition
@@ -71,16 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'superlists.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
