4b9771fae6ecb269338c5efba5db6daf2328455b
AfrosamuraiNo1 <alexkopenkin777@mail.ru>
2023-04-15 10:32:59 +0300

    Refactor hard-coded URLs out of temp(13.3 PATH)

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -96,3 +96,12 @@ class ListViewTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.post(f'/lists/{correct_list.id}/',data={'item_text': 'A new item for an existing list'})
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+    
+    def test_validation_errors_end_up_on_lists_page(self):
+        '''тест: ошибки валидации оканчиваются на странице списков'''
+        list_ = List.objects.create()
+        response = self.client.post(f'/lists/{list_.id}/',data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
